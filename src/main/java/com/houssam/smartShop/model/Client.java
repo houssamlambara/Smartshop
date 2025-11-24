9e047d449228da8963b5be86704bd76e456a347c
houssamlambara <lambarahoussam@gmail.com>
2025-11-24 19:10:13 +0100

    add model client

---
+++ src/main/java/com/houssam/smartShop/model/Client.java
@@ -0,0 +1,35 @@
+package com.houssam.smartShop.model;
+
+import com.houssam.smartShop.enums.CustomerTier;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private String id;
+    private String nom;
+    private String prenom;
+    private String email;
+    @Enumerated(EnumType.STRING)
+    private CustomerTier customerTier = CustomerTier.BASIC;
+    private Integer totalOrders = 0;
+    private BigDecimal totalSpent = BigDecimal.ZERO;
+    private LocalDate firstOrderDate;
+    private LocalDate lastOrderDate;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
---
+++ src/main/java/com/houssam/smartShop/model/User.java
@@ -0,0 +1,30 @@
+package com.houssam.smartShop.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.management.relation.Role;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+
+}
