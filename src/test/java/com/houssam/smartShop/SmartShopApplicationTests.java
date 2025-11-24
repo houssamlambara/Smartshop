72d919322f47712f8c774dbecd877e01de25687e
houssamlambara <lambarahoussam@gmail.com>
2025-11-24 10:27:05 +0100

    first commit

---
+++ src/test/java/com/houssam/smartShop/SmartShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houssam.smartShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
