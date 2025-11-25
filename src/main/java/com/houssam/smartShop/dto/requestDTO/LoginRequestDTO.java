82893e152f781e11f06241358f23e01f69428954
houssamlambara <lambarahoussam@gmail.com>
2025-11-25 15:19:11 +0100

    update DTO login

--- src/main/java/com/houssam/smartShop/dto/requestDTO/LoginRequestDTO.java
+++ src/main/java/com/houssam/smartShop/dto/requestDTO/LoginRequestDTO.java
@@ -1,9 +1,18 @@
 package com.houssam.smartShop.dto.requestDTO;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequestDTO {
+
+    @NotBlank(message = "Le username est obligatoire")
     private String username;
+
+    @NotBlank(message = "Le mot de passe est obligatoire")
     private String password;
 }
--- src/main/java/com/houssam/smartShop/dto/responseDTO/LoginResponseDTO.java
+++ src/main/java/com/houssam/smartShop/dto/responseDTO/LoginResponseDTO.java
@@ -1,10 +1,18 @@
 package com.houssam.smartShop.dto.responseDTO;
 
+import com.houssam.smartShop.enums.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginResponseDTO {
-    private String id;
+    private String userId;
     private String username;
-    private String role;
+    private UserRole role;
+    private String message;
 }
