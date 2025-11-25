2d711255d445d9a339d96ce313e93256cc83a567
houssamlambara <lambarahoussam@gmail.com>
2025-11-25 09:43:11 +0100

    ajout du loginDTO

---
+++ src/main/java/com/houssam/smartShop/dto/requestDTO/LoginRequestDTO.java
@@ -0,0 +1,9 @@
+package com.houssam.smartShop.dto.requestDTO;
+
+import lombok.Data;
+
+@Data
+public class LoginRequestDTO {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/houssam/smartShop/dto/responseDTO/LoginResponseDTO.java
@@ -0,0 +1,10 @@
+package com.houssam.smartShop.dto.responseDTO;
+
+import lombok.Data;
+
+@Data
+public class LoginResponseDTO {
+    private String id;
+    private String username;
+    private String role;
+}
