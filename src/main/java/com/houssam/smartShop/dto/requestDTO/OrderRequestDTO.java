9def1c8a9cd29f7039976e01a9725714cfca6576
houssamlambara <lambarahoussam@gmail.com>
2025-11-27 20:19:06 +0100

    update orderRequest et orderMapper

--- src/main/java/com/houssam/smartShop/dto/requestDTO/OrderRequestDTO.java
+++ src/main/java/com/houssam/smartShop/dto/requestDTO/OrderRequestDTO.java
@@ -3,7 +3,6 @@ package com.houssam.smartShop.dto.requestDTO;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,8 +20,15 @@ public class OrderRequestDTO {
 
     private String promoCode;
 
-    @NotNull(message = "La date de la commande est obligatoire")
-    private LocalDate dateCommande;
+//    @NotNull(message = "La date de la commande est obligatoire")
+//    private LocalDate dateCommande;
+//
+//    private Double sousTotal;
+//    private Double montantRemise;
+//    private Double montantHT;
+//    private Double tauxTVA;
+//    private Double montantTVA;
+//    private Double totalTTC;
 
     @NotEmpty(message = "La commande doit contenir au moins un article")
     @Valid
--- src/main/java/com/houssam/smartShop/mapper/OrderMapper.java
+++ src/main/java/com/houssam/smartShop/mapper/OrderMapper.java
@@ -4,11 +4,27 @@ import com.houssam.smartShop.dto.requestDTO.OrderRequestDTO;
 import com.houssam.smartShop.dto.responseDTO.OrderResponseDTO;
 import com.houssam.smartShop.model.Order;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "client", ignore = true)
+    @Mapping(target = "promoCode", ignore = true)
+    @Mapping(target = "sousTotal", ignore = true)
+    @Mapping(target = "montantRemise", ignore = true)
+    @Mapping(target = "montantHT", ignore = true)
+    @Mapping(target = "tauxTVA", ignore = true)
+    @Mapping(target = "montantTVA", ignore = true)
+    @Mapping(target = "totalTTC", ignore = true)
+    @Mapping(target = "statut", ignore = true)
+    @Mapping(target = "dateCommande", ignore = true)
+    @Mapping(target = "items", ignore = true)
     Order toEntity(OrderRequestDTO dto);
+
+    @Mapping(target = "clientId", source = "client.id")
+    @Mapping(target = "promoCodeId", source = "promoCode.id")
     OrderResponseDTO toResponse(Order order);
 
 }
