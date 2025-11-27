4eda4432cd465263d8825003958eedffd97ff89e
houssamlambara <lambarahoussam@gmail.com>
2025-11-27 20:20:05 +0100

    add logique metier Order

--- src/main/java/com/houssam/smartShop/service/OrderService.java
+++ src/main/java/com/houssam/smartShop/service/OrderService.java
@@ -2,10 +2,22 @@ package com.houssam.smartShop.service;
 
 import com.houssam.smartShop.dto.requestDTO.OrderRequestDTO;
 import com.houssam.smartShop.dto.responseDTO.OrderResponseDTO;
+import com.houssam.smartShop.enums.OrderStatus;
+
+import java.util.List;
 
 public interface OrderService {
 
     OrderResponseDTO createOrder(OrderRequestDTO dto);
 
+    OrderResponseDTO getOrderById(String id);
+
+    List<OrderResponseDTO> getAllOrders();
+
+    OrderResponseDTO updateOrder(String id, OrderRequestDTO dto);
+
+    void cancelOrder(String id);
+
+    void updateOrderStatus(String id, OrderStatus status);
 
     }
